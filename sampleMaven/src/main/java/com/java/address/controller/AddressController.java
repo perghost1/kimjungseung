f5c47ff2645211442a4c3248819186be2f52f079
perghost1 <perghost@icloud.com>
2015-06-12 14:31:21 +0900

    ver_201506120231

--- sampleMaven/src/main/java/com/java/address/controller/AddressController.java
+++ sampleMaven/src/main/java/com/java/address/controller/AddressController.java
@@ -1,8 +1,15 @@
 package com.java.address.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import com.java.address.service.AddressService;
 
 @Controller
 public class AddressController {
-
+	
+	@Autowired
+	private AddressService addService;
+	
+	
 }
--- sampleMaven/src/main/java/com/java/address/dao/AddressDaoImpl.java
+++ sampleMaven/src/main/java/com/java/address/dao/AddressDaoImpl.java
@@ -8,4 +8,8 @@ import com.java.address.dto.AddressDto;
 
 @Component
 public class AddressDaoImpl implements AddressDao {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
 }
